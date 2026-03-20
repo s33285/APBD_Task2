0ef3e8bb006d8b77deea01e81197d6b4c554c7b0
Tatiana Ivanova <s33285@pjwstk.edu.pl>
2026-03-20 17:59:09 +0100

    Created enums, corrected classes and made User

---
+++ Enum/EquipmentStatus.cs
@@ -0,0 +1,9 @@
+namespace APBD_TASK2.Enum
+{
+    public enum EquipmentStatus
+    {
+        Available,
+        Rented,
+        Unavailable
+    }
+}
---
+++ Models/Equipment.cs
@@ -0,0 +1,21 @@
+namespace APBD_TASK2.Models
+{
+    public abstract class Equipment {
+        private static int _nextId = 1;
+        public int Id{  get; set; }
+        public string Name{ get; set; }
+        public string Description { get; set; }
+        public DateTime AddedDate { get; set; }
+        public EquipmentStatus Status { get; set; }
+
+        protected Equipment(string name, string description, DateTime addedDate) {
+            Id = _nextId++;
+            Name = name;
+            Description = description;
+            AddedDate = addedDate;
+            Status = EquipmentStatus.Available;
+        }
+
+    
+    }
+}
